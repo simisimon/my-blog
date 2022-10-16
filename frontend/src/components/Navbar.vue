1e7a58d4dc7317b6f69a6da24418ac12cc0dc504
Sebastian Simon <Bastisimon95@googlemail.com>
2022-10-16 09:38:14 +0200

    Revise navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -9,24 +9,12 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon @click="redirectToUrl('https://github.com/simisimon')">
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
-
-      <v-btn icon @click="redirectToUrl('https://twitter.com/SimiSimon17')">
-        <v-img src="../assets/twitter-white.png" max-height="22" max-width="22"></v-img>
-      </v-btn>
-
-      <v-btn icon @click="redirectToUrl('https://www.linkedin.com/in/sebastian-simon-8baa75191/')">
-        <v-icon>mdi-linkedin</v-icon>
-      </v-btn>
-
-      <v-btn icon @click="redirectToUrl('https://www.xing.com/profile/Sebastian_Simon65/cv')" >
-        <v-icon>mdi-xing</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
       </v-btn>
 
       <v-btn icon>
-        <v-icon>mdi-home</v-icon>
+        <v-icon>mdi-account-card-outline</v-icon>
       </v-btn>
     </v-app-bar>
   </div>
