faacef54b587aeeced4c18b0d1750185fee3a6a8
Sebastian Simon <Bastisimon95@googlemail.com>
2022-12-29 14:52:49 +0100

    start with blog layout

--- frontend/src/components/Blog.vue
+++ frontend/src/components/Blog.vue
@@ -1,8 +1,33 @@
 <template>
-    <div class="main-container"></div>
+    <div class="main-container">
+        <div class="intro">
+
+        </div>
+        <div class="recent-posts">
+            <div class="container">
+
+                
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+export default {
+    name: 'Navbar',
+
+    data: () => ({
+        drawer: null,
+        posts: [
+            { title: 'This is my first blog post', img: '', tags: ["sport", "fitness"], content: "Lorem ipsum", author: "Sebastian Simon"},
+            { title: 'This is my second blog post', img: '', tags: ["gaming", "computer"], content: "Lorem ipsum", author: "Sebastian Simon"},
+            { title: 'This is my third blog post', img: '', tags: ["real life story"], content: "Lorem ipsum", author: "Sebastian Simon" },
+            { title: 'This is my fourth blog post', img: '', tags: ["test"], content: "Lorem ipsum", author: "Sebastian Simon" },
+            { title: 'This is my sixth blog post', img: '', tags: ["test"], content: "Lorem ipsum", author: "Sebastian Simon" },
+        ],
+    }),
+}
 
 </script>
 
